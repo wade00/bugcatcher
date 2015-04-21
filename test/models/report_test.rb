b8ec0f482c724f202dbc1a61fcbdd26f543e59f2
Wade Novak <wade.novak@gmail.com>
2015-04-21 16:13:24 -0500

    Creates report model tests

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "report saved" do
+    original_report_total = Report.count
+    r = Report.new(content: "New report")
+    r.save
+    assert_equal (original_report_total + 1), Report.count
+  end
+
+  test "accepted report passes accepted? method" do
+    r = reports(:first_report)
+    assert r.accepted_by_project_owner == nil
+    r.accepted_by_project_owner = true
+    r.save
+    assert r.accepted? == true
+  end
+
+  test "denied report fails accepted? method" do
+    r = reports(:first_report)
+    assert r.accepted_by_project_owner == nil
+    r.accepted_by_project_owner = false
+    r.save
+    assert r.accepted? == false
+  end
 end
