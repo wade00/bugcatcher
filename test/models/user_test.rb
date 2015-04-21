ba4a905f4041be5a7fe0c43b4f88cfd2ed02564c
Wade Novak <wade.novak@gmail.com>
2015-04-21 15:08:04 -0500

    Creates user  model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,10 +2,18 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  # test "the truth" do
-  #   user = users(:daniel)
-  #   puts User.first
-  #   assert_equal true, user.valid?
-  # end
+  test "valid user saved" do
+    original_user_total = User.count
+    u = User.new(name: "Don", email: "donald@example.com", password: "12341234", password_confirmation: "12341234")
+    u.save
+    assert_equal (original_user_total + 1), User.count
+  end
+
+  test "invalid user not saved" do
+    original_user_total = User.count
+    u = User.new(name: "Don", email: "donald@example.com")
+    u.save
+    assert_equal (original_user_total), User.count
+  end
 
 end
