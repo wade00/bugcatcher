ceb00779cf1f99336edc4ed0c2f2be6514f67030
Wade Novak <wade.novak@gmail.com>
2015-04-27 16:23:06 -0500

    Improves reports test coverage

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -1,26 +1,19 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  test "report saved" do
-    original_report_total = Report.count
-    r = Report.new(content: "New report")
-    r.save
-    assert_equal (original_report_total + 1), Report.count
-  end
-
   test "accepted report passes accepted? method" do
     r = reports(:first_report)
-    assert r.accepted_by_project_owner == nil
+    assert_not r.accepted?
     r.accepted_by_project_owner = true
     r.save
-    assert r.accepted? == true
+    assert r.accepted?
   end
 
   test "denied report fails accepted? method" do
     r = reports(:first_report)
-    assert r.accepted_by_project_owner == nil
+    assert_not r.accepted?
     r.accepted_by_project_owner = false
     r.save
-    assert r.accepted? == false
+    assert_not r.accepted?
   end
 end
