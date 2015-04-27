c1b2079ae87e3be2a5f114fdbe7f8c8c61d92d41
Wade Novak <wade.novak@gmail.com>
2015-04-27 16:26:50 -0500

    Syncs with master user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,22 +1,6 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-
-<<<<<<< HEAD
-  test "valid user saved" do
-    original_user_total = User.count
-    u = User.new(name: "Don", email: "donald@example.com", password: "12341234", password_confirmation: "12341234")
-    u.save
-    assert_equal (original_user_total + 1), User.count
-  end
-
-  test "invalid user not saved" do
-    original_user_total = User.count
-    u = User.new(name: "Don", email: "donald@example.com")
-    u.save
-    assert_equal (original_user_total), User.count
-  end
-=======
   def test_if_user_valid
     user = users(:marcus)
 
@@ -29,6 +13,5 @@ class UserTest < ActiveSupport::TestCase
   #   puts User.first
   #   assert_equal true, user.valid?
   # end
->>>>>>> upstream/master
 
 end
