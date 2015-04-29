cfee2010d9ca77a9e670293edbb0f7ae1ac76d46
Wade Novak <wade.novak@gmail.com>
2015-04-29 09:00:08 -0500

    Adds report and project test coverage

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -9,4 +9,35 @@ class ProjectTest < ActiveSupport::TestCase
   # would never reported the bad UX. Everything else is straightforward and
   # easy to fix if you let a mistake pass.
 
+  test "project name cannont be blank" do
+    project_count = Project.count
+    p1 = Project.new(owner: users(:daniel),
+                     name: '',
+                     description: 'Teach your best class')
+
+    assert(!p1.valid?)
+    assert(p1.errors.messages[:name].present?)
+
+    p1.save
+    assert_equal Project.count, project_count
+  end
+
+  test "user can't have two project with same name" do
+    project_count = Project.count
+    p1 = Project.new(owner: users(:daniel),
+                     name: 'Test Project',
+                     description: 'Teach your best class')
+
+    p1.save
+    assert_equal Project.count, (project_count + 1)
+
+    p2 = Project.new(owner: users(:daniel),
+                     name: 'Test Project',
+                     description: 'Different description')
+
+    assert(!p2.valid?)
+    assert(p2.errors.messages[:name].present?)
+    assert_equal Project.count, (project_count + 1)
+  end
+
 end
